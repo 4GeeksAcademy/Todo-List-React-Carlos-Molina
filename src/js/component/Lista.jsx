0842e503faae1398e77b4c035f6c743957ff84ea
Carlos Molina <carlosm12.inves@gmail.com>
2024-08-11 11:58:54 +0000

    ToDo List OK

--- src/js/component/Lista.jsx
+++ src/js/component/Lista.jsx
@@ -1,45 +1,67 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function Lista() {
 	const [tareas, setTareas] = useState([]);
 	const [tareaInput, setTareaInput] = useState('');
-  
+	const [hoverIndex, setHoverIndex] = useState(null);
+
 	const actualizarTarea = (push) => {
-	  setTareaInput(push.target.value);
+		setTareaInput(push.target.value);
 	};
-  
+
 	const añadirTarea = () => {
-	  if (tareaInput.trim()) {
-		setTareas([...tareas, tareaInput]);
-		setTareaInput('');
-	  }
+		if (tareaInput.trim()) {
+			setTareas([...tareas, tareaInput]);
+			setTareaInput('');
+		}
 	};
-  
+
+	const manejarKeyDown = (push) => {
+		if (push.key === 'Enter') {
+			añadirTarea();
+		}
+	};
+
 	const eliminarTarea = (index) => {
-	  const nuevaTarea = tareas.filter((_, i) => i !== index);
-	  setTareas(nuevaTarea);
+		const nuevaTarea = tareas.filter((_, i) => i !== index);
+		setTareas(nuevaTarea);
 	};
-  
+
 	return (
-	  <div className="App">
-		<h1>Lista de Tareas</h1>
-		<input 
-		  type="text" 
-		  value={tareaInput} 
-		  onChange={actualizarTarea} 
-		  placeholder="Añadir nueva tarea" 
-		/>
-		<button onClick={añadirTarea}>Añadir</button>
-		<ul>
-		  {tareas.map((tarea, index) => (
-			<li key={index}>
-			  {tarea}
-			  <button onClick={() => eliminarTarea(index)}>Eliminar</button>
-			</li>
-		  ))}
-		</ul>
-	  </div>
+		<div className="body"> 
+			<h1 style={{fontSize:'330%'}}>Lista de Tareas</h1>
+			<div className="lista">
+				<input
+					className="inputTarea"
+					type="text"
+					value={tareaInput}
+					onChange={actualizarTarea}
+					onKeyDown={manejarKeyDown}
+					placeholder="Añadir nueva tarea"
+				/>
+				<button className="añadir" onClick={añadirTarea}> ✔️​</button>
+				<ul>
+					{tareas.map((tarea, index) => (
+						<li
+							key={index}
+							onMouseEnter={() => setHoverIndex(index)}
+							onMouseLeave={() => setHoverIndex(null)}
+						>
+							{tarea}
+							{hoverIndex === index && (
+								<button className="borrar" onClick={() => eliminarTarea(index)}> ❌ ​</button>
+							)}
+							<hr style={{ margin: '5px 0' }} />
+						</li>
+					))}
+				</ul>
+				<div className="tareasRestantes">
+					Tareas restantes: {tareas.length}
+				</div>
+			</div>
+		</div>
 	);
-  }
-  
-  export default Lista;
+}
+
+export default Lista;
+
